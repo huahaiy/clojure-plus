279364836fdcfbe9d531599a446a0fb1e866e8ff
Huahai Yang <hyang@juji-inc.com>
2025-03-07 13:44:09 -0800

    try own release

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject io.github.tonsky/clojure-plus "0.0.0"
+(defproject org.clojars.huahaiy/clojure-plus "1.1.1"
   :description "A collection of utilities that improve Clojure experience"
   :license     {:name "MIT" :url "https://github.com/tonsky/clojure-plus/blob/master/LICENSE"}
   :url         "https://github.com/tonsky/clojure-plus"
   :dependencies
   [[org.clojure/clojure "1.12.0"]]
-  :deploy-repositories
-  {"clojars"
-   {:url "https://clojars.org/repo"
-    :username "tonsky"
-    :password :env/clojars_token
-    :sign-releases false}})
+  :deploy-repositories [["clojars" {:url           "https://repo.clojars.org"
+                                    :username      :env/clojars_username
+                                    :password      :env/clojars_password
+                                    :sign-releases false}]])
